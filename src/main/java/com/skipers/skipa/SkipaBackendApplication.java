5501bed210acede8c5b1659735c30665be32692b
Junghwa <jungh1214@gmail.com>
2026-05-21 17:06:23 +0900

    initial commit

---
+++ src/main/java/com/skipers/skipa/SkipaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skipers.skipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skipers/skipa/SkipaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skipers.skipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkipaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
