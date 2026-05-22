f9e72ab067d1ac9155b797a6dfbd5bc8f648b9b5
Junghwa <jungh1214@gmail.com>
2026-05-22 15:10:24 +0900

    feat: add common BaseEntity

--- src/main/java/com/skipers/skipa/global/common/entity/BaseEntity.java
+++ src/main/java/com/skipers/skipa/global/common/entity/BaseEntity.java
@@ -1,4 +1,28 @@
 package com.skipers.skipa.global.common.entity;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
 }
