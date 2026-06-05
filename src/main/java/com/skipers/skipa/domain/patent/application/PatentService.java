646f34003e100214e63d62ffda77fee525c4e42f
Gilhoon Ko <rhrlfgns89@gmail.com>
2026-06-05 09:53:35 +0900

    refactor: clean unused import in patent service

--- src/main/java/com/skipers/skipa/domain/patent/application/PatentService.java
+++ src/main/java/com/skipers/skipa/domain/patent/application/PatentService.java
@@ -26,8 +26,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
